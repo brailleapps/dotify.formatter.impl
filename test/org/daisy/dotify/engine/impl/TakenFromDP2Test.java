93047a1dd2bc863c211029f5204559a658616e30
Bert Frees <bertfrees@gmail.com>
2016-05-11 18:36:30 +0200

    Add a test for issue #194
    see https://github.com/joeha480/dotify/issues/194

--- test/org/daisy/dotify/engine/impl/TakenFromDP2Test.java
+++ test/org/daisy/dotify/engine/impl/TakenFromDP2Test.java
@@ -346,6 +346,11 @@ public class TakenFromDP2Test extends AbstractFormatterEngineTest {
 	public void testBlockUnderlineWithChildBlock() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/dp2/block-underline-with-child-block-input.obfl",
 		        "resource-files/dp2/block-underline-with-child-block-expected.pef", false);
+	@Ignore // see https://github.com/joeha480/dotify/issues/194
+	@Test
+	public void testMarkerIndicatorBeforeBlock() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
+		testPEF("resource-files/dp2/marker-indicator-before-block-input.obfl",
+		        "resource-files/dp2/marker-indicator-before-block-expected.pef", true);
 	}
 	@Ignore // see https://github.com/joeha480/dotify/issues/196
 	@Test

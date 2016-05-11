fa5ba7a420ea117f804d9c20cffc9ee226851ccc
Bert Frees <bertfrees@gmail.com>
2016-05-11 18:37:27 +0200

    Add a test for issue #196
    see https://github.com/joeha480/dotify/issues/196

--- test/org/daisy/dotify/engine/impl/TakenFromDP2Test.java
+++ test/org/daisy/dotify/engine/impl/TakenFromDP2Test.java
@@ -346,5 +346,10 @@ public class TakenFromDP2Test extends AbstractFormatterEngineTest {
 	public void testBlockUnderlineWithChildBlock() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/dp2/block-underline-with-child-block-input.obfl",
 		        "resource-files/dp2/block-underline-with-child-block-expected.pef", false);
+	@Ignore // see https://github.com/joeha480/dotify/issues/196
+	@Test
+	public void testRowSpacingAndFooter() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
+		testPEF("resource-files/dp2/row-spacing-and-footer-input.obfl",
+		        "resource-files/dp2/row-spacing-and-footer-expected.pef", true);
 	}
 }

2547c9051ea39d69920b40f7139db7ab3296b881
Daniel Persson <kalaspuffar@users.noreply.github.com>
2019-08-15 15:04:27 +0200

    Change to repeating phrasing

--- src/org/daisy/dotify/formatter/impl/search/VolumeKeepPriority.java
+++ src/org/daisy/dotify/formatter/impl/search/VolumeKeepPriority.java
@@ -119,8 +119,7 @@ public final class VolumeKeepPriority implements Comparable<VolumeKeepPriority> 
 	 *
 	 * <p>Objects are ordered according to {@link #compareTo(VolumeKeepPriority)}, from lower to higher priority.</p>
 	 *
-	 * @return a negative integer, zero, or a positive integer as the first argument is less
-	 *         than, equal to, or greater than the second.
+	 * @return a comparator that imposes the natural ordering on {@link VolumeKeepPriority} objects.
 	 */
 	public static Comparator<VolumeKeepPriority> naturalOrder() {
 		if (comparator == null) {

4eee8aefbd24f46d432d882da98093ec3615009b
PaulRambags <paulrambags@dedicon.nl>
2019-09-27 08:49:58 +0200

    #106: Do not keep temporary test file

--- integrationtest/org/daisy/dotify/formatter/test/TakenFromDP2Test.java
+++ integrationtest/org/daisy/dotify/formatter/test/TakenFromDP2Test.java
@@ -336,11 +336,11 @@ public class TakenFromDP2Test extends AbstractFormatterEngineTest {
 	@Test
 	public void testTocTextDeprecated() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/dp2/toc-text-input-deprecated.obfl",
-		        "resource-files/dp2/toc-text-expected.pef", true);
+		        "resource-files/dp2/toc-text-expected.pef", false);
 	}
 	@Test
 	public void testTocText() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/dp2/toc-text-input.obfl",
-		        "resource-files/dp2/toc-text-expected.pef", true);
+		        "resource-files/dp2/toc-text-expected.pef", false);
 	}
 }
